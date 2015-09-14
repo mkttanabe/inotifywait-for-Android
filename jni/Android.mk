f91ebbadd32585f970f1a456b75e7e20ce10fbe3
TANABE Makoto <mkttanabe2012@gmail.com>
2015-09-14 17:20:45 +0900

    for Android 5.0 or later

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS   = -std=c99
+# build PIE (for Android 4.1.2 - 4.4 / 5.0 or later)
+LOCAL_CFLAGS += -fPIE -std=c99
+LOCAL_LDFLAGS += -fPIE -pie
+
+# build non-PIE (for Android 4.0.3 or earlier)
+#LOCAL_CFLAGS   = -std=c99
 
 LOCAL_C_INCLUDES := \
 	./jni/regex \
